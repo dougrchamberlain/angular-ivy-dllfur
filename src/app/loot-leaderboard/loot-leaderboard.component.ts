5990f84d9e2f5516b48f79ee77d2b8dfc582891f
Doug Chamberlain <dougrchamberlain@gmail.com>
2021-06-22 12:39:53 -0400

    wired up to run locally

--- src/app/loot-leaderboard/loot-leaderboard.component.ts
+++ src/app/loot-leaderboard/loot-leaderboard.component.ts
@@ -1,18 +1,16 @@
-import firebase from 'firebase/app';
-import databse from 'firebase/database';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import firebase from "firebase/app";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
-// Firebase App (the core Firebase SDK) is always required and must be listed first
+// firebase App (the core Firebase SDK) is always required and must be listed first
 
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// if you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
 // import * as firebase from "firebase/app"
 
-const CURRENT_APP = 'pirates-friend';
 @Component({
-  selector: 'app-loot-leaderboard',
-  templateUrl: './loot-leaderboard.component.html',
-  styleUrls: ['./loot-leaderboard.component.css']
+  selector: "app-loot-leaderboard",
+  templateUrl: "./loot-leaderboard.component.html",
+  styleUrls: ["./loot-leaderboard.component.css"]
 })
 export class LootLeaderboardComponent implements OnInit {
   loot: any = [];
@@ -21,28 +19,28 @@ export class LootLeaderboardComponent implements OnInit {
   app: any;
   database: any;
   constructor(private readonly http: HttpClient) {
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    // your web app's Firebase configuration
+    // for Firebase JS SDK v7.20.0 and later, measurementId is optional
+    // your web app's Firebase configuration
+    // for Firebase JS SDK v7.20.0 and later, measurementId is optional
+    // for Firebase JS SDK v7.20.0 and later, measurementId is optional
   }
 
-  //when you comeback to this set it up in a local folder and use firebase cli
-  ngOnInit() {
-    const firebaseConfig = {
-      apiKey: 'AIzaSyAnPCD6oKP_0XuT5ptHlXvNIZlmr1Abuo4',
-      authDomain: 'pirates-friend.firebaseapp.com',
-      databaseURL: 'https://pirates-friend-default-rtdb.firebaseio.com',
-      projectId: 'pirates-friend',
-      storageBucket: 'pirates-friend.appspot.com',
-      messagingSenderId: '311560368568',
-      appId: ':311560368568:web:0ca4596c7a1215fa76df4d',
-      measurementId: 'G-FQDEYWJCQR'
+  // when you comeback to this set it up in a local folder and use firebase cli
+  ngOnInit(): void {
+    const firebaseConfig: any = {
+      apiKey: "AIzaSyAnPCD6oKP_0XuT5ptHlXvNIZlmr1Abuo4",
+      authDomain: "pirates-friend.firebaseapp.com",
+      databaseURL: "https://pirates-friend-default-rtdb.firebaseio.com",
+      projectId: "pirates-friend",
+      storageBucket: "pirates-friend.appspot.com",
+      messagingSenderId: "311560368568",
+      appId: ":311560368568:web:0ca4596c7a1215fa76df4d",
+      measurementId: "G-FQDEYWJCQR"
     };
-    // Initialize Firebase
+    // initialize Firebase
 
-    const firebaseApp = firebase.initializeApp(firebaseConfig);
+    const firebaseApp: any = firebase.initializeApp(firebaseConfig);
 
     this.database = firebase.database(firebaseApp);
     // firebase.analytics();
@@ -50,13 +48,13 @@ export class LootLeaderboardComponent implements OnInit {
     console.log(this.database);
   }
 
-  //make this function work
-  writeUserData() {
+  // make this function work
+  writeUserData(): void {
     firebase
       .database()
-      .ref('users/2')
+      .ref("users/2")
       .set({
-        username: 'doug'
+        username: "doug"
       });
   }
 }
